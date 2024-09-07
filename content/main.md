692ccb891ef4e24a300e3226cf103471086b68ea
jacobshirley <jakeshirley2@gmail.com>
2024-09-07 12:54:27 +0100

    chore: first commit

---
+++ content/main.md
@@ -0,0 +1,6 @@
++++
+title = 'Main'
+date = 2024-09-07T10:50:41+01:00
+draft = true
++++
+# test
---
+++ themes/jake/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./layouts/**/*.{html,js}"],
+    theme: {
+        fontFamily: {
+            sans: ['Poppins', 'sans-serif'],
+        }
+    }
+}
