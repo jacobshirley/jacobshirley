2cc9f4fdac7b9b4dff4aebb0242f80dacdb7bba4
jacobshirley <jakeshirley2@gmail.com>
2024-09-17 22:00:13 +0100

    chore: initial layout done

--- themes/jake/tailwind.config.js
+++ themes/jake/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./layouts/**/*.{html,js}"],
-    theme: {
-        fontFamily: {
-            sans: ['Poppins', 'sans-serif'],
-        }
-    }
-}
+  content: ["./layouts/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+      serif: ["Outfit", "sans-serif"],
+    },
+    screens: {
+      xl: "1440px",
+    },
+    colors: {
+      white: "#FFFFFF",
+      blue: "#7691FF",
+      black: "#000000",
+      lightgrey: "#F1F1F1",
+    },
+  },
+};
